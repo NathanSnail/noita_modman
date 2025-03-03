f5fee591df30df4d986093fbc6eed0942697f009
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-03-03 16:29:01 +1300

    move string byte read/write out

--- src/ext.rs
+++ src/ext.rs
@@ -1,11 +1,23 @@
-use std::io::{Read, Write};
+use std::{
+    fmt::{Debug, Display},
+    io::{Read, Write},
+};
 
 use anyhow::{anyhow, Context};
 use bytemuck::{AnyBitPattern, NoUninit};
 
+pub enum Endianness {
+    Little,
+    Big,
+}
+
 pub trait ByteReaderExt {
     fn read_le<T: AnyBitPattern>(&mut self) -> anyhow::Result<T>;
     fn read_be<T: AnyBitPattern>(&mut self) -> anyhow::Result<T>;
+    fn read_str<T: AnyBitPattern>(&mut self, endianness: Endianness) -> anyhow::Result<String>
+    where
+        <T as TryInto<usize>>::Error: Debug,
+        usize: TryFrom<T>;
 }
 
 impl<R: Read> ByteReaderExt for R {
@@ -15,6 +27,32 @@ impl<R: Read> ByteReaderExt for R {
             .context(format!("Reading {} bytes into buffer", buffer.capacity()))?;
         Ok(*(bytemuck::try_from_bytes::<T>(&buffer)
             .map_err(|e| anyhow!("Failed to try from bytes {e}"))?))
+    }
+
+    fn read_str<T: AnyBitPattern>(&mut self, endianness: Endianness) -> anyhow::Result<String>
+    where
+        <T as TryInto<usize>>::Error: Debug,
+        T: TryInto<usize>,
+    {
+        let len: usize = match endianness {
+            Endianness::Little => self
+                .read_le::<T>()
+                .context("Reading string length")?
+                .try_into()
+                .expect("Length must be able to be cast to usize"), // TODO: make this anyhow / comptime ideally
+            Endianness::Big => self
+                .read_be::<T>()
+                .context("Reading string length")?
+                .try_into()
+                .expect("Length must be able to be cast to usize"),
+        };
+
+        let mut buf = vec![0; len as usize];
+        self.read_exact(&mut buf).context("Reading string data")?;
+        Ok(String::from_utf8(buf.clone()).context(
+            // TODO: another wasteful clone
+            format!("Converting string data {:?} to utf8", buf),
+        )?)
     }
 
     fn read_be<T: AnyBitPattern>(&mut self) -> anyhow::Result<T> {
@@ -30,6 +68,11 @@ impl<R: Read> ByteReaderExt for R {
 pub trait ByteWriterExt {
     fn write_le<T: NoUninit>(&mut self, value: T) -> anyhow::Result<()>;
     fn write_be<T: NoUninit>(&mut self, value: T) -> anyhow::Result<()>;
+    fn write_str<T: NoUninit>(&mut self, value: &str, endianness: Endianness) -> anyhow::Result<()>
+    where
+        <usize as TryInto<T>>::Error: Debug,
+        T: Display,
+        T: TryFrom<usize>;
 }
 
 impl<W: Write> ByteWriterExt for W {
@@ -46,6 +89,26 @@ impl<W: Write> ByteWriterExt for W {
         bytes_mut.reverse();
         self.write_all(&bytes_mut)
             .context(format!("Writing {bytes:?} to the buffer"))?;
+        Ok(())
+    }
+
+    fn write_str<T: NoUninit>(&mut self, value: &str, endianness: Endianness) -> anyhow::Result<()>
+    where
+        <usize as TryInto<T>>::Error: Debug,
+        T: Display,
+        T: TryFrom<usize>,
+    {
+        let len: T = value
+            .len()
+            .try_into()
+            .expect("Length should be able to cast to T");
+        match endianness {
+            Endianness::Little => self.write_le(len),
+            Endianness::Big => self.write_be(len),
+        }
+        .context(format!("Writing string length {len}"))?;
+        self.write_all(value.as_bytes())
+            .context("Writing string body")?;
         Ok(())
     }
 }
