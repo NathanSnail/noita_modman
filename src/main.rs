8b19ded4d2f88749aca32c7bb82c248a6b8cc7b5
Nathan <nathan.titirangi@gmail.com>
2025-06-10 00:00:35 +1200

    serde toml for config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::{marker::PhantomData, path::Path};
+use serde::Deserialize;
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    marker::PhantomData,
+    path::Path,
+};
+use toml;
 
 #[cfg(test)]
 extern crate quickcheck;
 #[cfg(test)]
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
+
+#[derive(Deserialize)]
+struct Config {
+    save00_path: String,
+    mods_path: String,
+    workshop_path: String,
+}
 
 mod app;
 mod collapsing_ui;
@@ -16,12 +30,17 @@ use app::{App, ProfilerInfo};
 use r#mod::Mod;
 
 fn main() -> anyhow::Result<()> {
-    let mod_config = Path::new("/home/nathan/.local/share/Steam/steamapps/compatdata/881100/pfx/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save00/mod_config.xml");
-    let mod_settings = Path::new("/home/nathan/.local/share/Steam/steamapps/compatdata/881100/pfx/drive_c/users/steamuser/AppData/LocalLow/Nolla_Games_Noita/save00/mod_settings.bin");
-    // let mod_settings = Path::new("./saved_settings");
-    let mods_dir = Path::new("/home/nathan/.local/share/Steam/steamapps/common/Noita/mods");
-    let workshop_dir =
-        Path::new("/home/nathan/.local/share/Steam/steamapps/workshop/content/881100");
+    let mut content_str = String::new();
+    let _ = &BufReader::new(
+        File::open(Path::new("./Config.toml").to_path_buf()).context("Reading config file")?,
+    )
+    .read_to_string(&mut content_str)
+    .context("Reading config to string")?;
+    let config: Config = toml::from_str(&content_str).context("Parsing config")?;
+    let mod_config = Path::new(&config.save00_path).join("mod_config.xml");
+    let mod_settings = Path::new(&config.save00_path).join("mod_settings.bin");
+    let mods_dir = Path::new(&config.mods_path);
+    let workshop_dir = Path::new(&config.workshop_path);
     #[cfg(feature = "profiler")]
     let profiler = ProfilerInfo {
         frame_counter: 0,
@@ -39,7 +58,7 @@ fn main() -> anyhow::Result<()> {
         &mod_config,
         Some(workshop_dir),
         Some(mods_dir),
-        mod_settings,
+        &mod_settings,
         profiler,
     )
     .context("Creating app")?;
